55e3f290220ff5fae07704a64037475b453d084b
Julinho Santos <julinhodev@gmail.com>
2021-12-14 13:44:03 -0300

    Add responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { ImGithub } from 'react-icons/im';
 const Footer = () => {
     return(
         <div className="footer-container">
-            <a href="https://github.com/julinhodev/" target="_blank" className="footer-github">
+            <a href="https://github.com/julinhodev/" rel="noreferrer" target="_blank" className="footer-github">
                 <ImGithub size={15} color="#778085"/>
                 Julinhodev
             </a>
